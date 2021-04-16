1a0542393210ba79d8460057261dc2bfe89df306
Denis <alehin.de@gmail.com>
2021-04-16 07:14:57 +0300

    Done virsion with google search

--- TaxiComparison/app/src/main/java/com/taxicomparison/taxicomparison/MainActivity.java
+++ TaxiComparison/app/src/main/java/com/taxicomparison/taxicomparison/MainActivity.java
@@ -1,5 +1,6 @@
 package com.taxicomparison.taxicomparison;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -20,8 +21,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -42,19 +48,11 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private SearchView searchAddressFrom;
-    private SearchView searchAddressTo;
-    private ListView listViewResFrom;
-    private ListView listViewResTo;
     private ListView listViewResult;
     private ProgressBar progressBar;
     private  static Context context;
-    final Address[] addressFrom = new Address[1];
-    final Address[] addressTo = new Address[1];
-
-    ArrayAdapter<String> adapterSearchAddressResFrom;
-    ArrayAdapter<String> adapterSearchAddressResTo;
-
+    Place depAddress;
+    Place destAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,48 +60,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
         initializeFields();
-        createListeners();
-        Places.initialize(context, "AIzaSyDq6GUrEFzocsgbecJWTOpecf7tJcycNCo");
-        PlacesClient placesClient = Places.createClient(this);
+
     }
 
-    public void onBtnGetResult(View view) {
-        hideKeyBoard();
-        if(isInputWrong()) {
-            return;
-        }
-        clearResultListView();
-        progressBar.setVisibility(View.VISIBLE);
-
-        makeHttpRequest();
-    }
     private void initializeFields() {
-        searchAddressFrom = findViewById(R.id.searchViewAddressFrom);
-        searchAddressTo = findViewById(R.id.searchViewAddressTo);
         listViewResult = findViewById(R.id.listViewResult);
         progressBar = findViewById(R.id.progressBar);
-        listViewResFrom = findViewById(R.id.listViewResFrom);
-        listViewResTo = findViewById(R.id.listViewResTo);
+        initializeDepartureAddress();
+        initializeDestinationAddress();
     }
 
-    private void hideKeyBoard() {
-        InputMethodManager imm = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
-        if (null != this.getCurrentFocus())
-            imm.hideSoftInputFromWindow(this.getCurrentFocus()
-                    .getApplicationWindowToken(), 0);
+    private void initializeDepartureAddress() {
+        Places.initialize(context, "AIzaSyDq6GUrEFzocsgbecJWTOpecf7tJcycNCo", new Locale("RU"));
+        PlacesClient placesClient = Places.createClient(this);
+        AutocompleteSupportFragment fragDepAddress = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.departureAddress);
+
+        fragDepAddress.setTypeFilter(TypeFilter.ADDRESS);
+        fragDepAddress.setCountries("Ru");
+        fragDepAddress.setPlaceFields(Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG));
+        fragDepAddress.setHint("Откуда");
+        fragDepAddress.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                depAddress = place;
+                fragDepAddress.setText(getAddressFromPlace(depAddress));
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
     }
 
-    private boolean isInputWrong() {
-        if(searchAddressFrom.getQuery().toString().length() == 0
-                || searchAddressTo.getQuery().toString().length() == 0 ) {
-            Toast.makeText(this, "Заполните поля", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        if(!addressFrom[0].getLocality().equals(addressTo[0].getLocality())) {
-            Toast.makeText(this, "Выбранны адреса в разных городах", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return false;
+    private void initializeDestinationAddress() {
+        AutocompleteSupportFragment fragDestAddress = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.destinationAddress);
+
+        fragDestAddress.setTypeFilter(TypeFilter.ADDRESS);
+        fragDestAddress.setCountries("Ru");
+        fragDestAddress.setPlaceFields(Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG));
+        fragDestAddress.setHint("Куда");
+        fragDestAddress.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                destAddress = place;
+                fragDestAddress.setText(getAddressFromPlace(destAddress));
+                clearResultListView();
+                progressBar.setVisibility(View.VISIBLE);
+                makeHttpRequest();
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
     }
 
     private void makeHttpRequest() {
@@ -150,13 +163,14 @@ public class MainActivity extends AppCompatActivity {
 
     private String buildRequestUrl() {
         String result = "";
-        String address1 = addressFrom[0].getThoroughfare() + " " + addressFrom[0].getSubThoroughfare();
-        String address2 = addressTo[0].getThoroughfare() + " " + addressFrom[0].getSubThoroughfare();
+        System.out.println(getCityFromPlace(depAddress));
+        System.out.println(getAddressFromPlace(depAddress));
+        System.out.println(getAddressFromPlace(destAddress));
         try {
             result = "https://20210415t110208-dot-taxiapi-310121.oa.r.appspot.com/"
-                    + "?city=" + URLEncoder.encode(addressFrom[0].getLocality(),"utf-8")
-                    + "?addres1=" + URLEncoder.encode(address1,"utf-8")
-                    +  "?address2=" + URLEncoder.encode(address2,"utf-8");
+                    + "?city=" + URLEncoder.encode(getCityFromPlace(depAddress),"utf-8")
+                    + "?addres1=" + URLEncoder.encode(getAddressFromPlace(depAddress),"utf-8")
+                    +  "?address2=" + URLEncoder.encode(getAddressFromPlace(destAddress),"utf-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -176,133 +190,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void createSearchListeners() {
-        Geocoder geocoder = new Geocoder(context, getRusLocal());
-        final Thread[] searchThread = {null};
-
-        searchAddressFrom.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                searchAddressFrom.setQuery(addressFrom[0].getAddressLine(0), false);
-                listViewResFrom.setAdapter(null);
-                return false;
+    private String getCityFromPlace(Place place) {
+        String result = "";
+        Geocoder gcd = new Geocoder(context, new Locale("RU"));
+        try {
+            List<Address> addresses = gcd.getFromLocation(place.getLatLng().latitude,
+                            place.getLatLng().longitude, 1);
+            if (addresses.size() != 0) {
+                result = addresses.get(0).getLocality();
             }
-
-            @Override
-            public boolean onQueryTextChange(String search) {
-                if(searchThread[0] != null) {
-                    searchThread[0].interrupt();
-                    searchThread[0] = null;
-                }
-                searchThread[0] = new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            List<Address> addresses = geocoder.getFromLocationName(search, 1);
-                            if(addresses.size() == 0) {
-                                return;
-                            }
-                            addressFrom[0] = addresses.get(0);
-                            ArrayList<String> strAddresses = new ArrayList<>();
-                            strAddresses.add(addressFrom[0].getAddressLine(0));
-                            adapterSearchAddressResFrom = new ArrayAdapter<String>(
-                                    context,
-                                    android.R.layout.simple_list_item_1,
-                                    strAddresses
-                            );
-                            MainActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    listViewResFrom.setAdapter(adapterSearchAddressResFrom);
-                                }
-                            });
-
-                        } catch (IOException e) {
-                            Toast.makeText(context, "Ничего не найдено", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-                searchThread[0].start();
-                return false;
-            }
-        });
-
-        searchAddressTo.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                searchAddressTo.setQuery(addressTo[0].getAddressLine(0), false);
-                listViewResTo.setAdapter(null);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String search) {
-                if(searchThread[0] != null) {
-                    searchThread[0].interrupt();
-                    searchThread[0] = null;
-                }
-                searchThread[0] = new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            List<Address> addresses = geocoder.getFromLocationName(search, 1);
-                            if(addresses.size() == 0) {
-                                return;
-                            }
-                            addressTo[0] = addresses.get(0);
-                            ArrayList<String> strAddresses = new ArrayList<>();
-                            strAddresses.add(addressTo[0].getAddressLine(0));
-                            adapterSearchAddressResTo = new ArrayAdapter<String>(
-                                    context,
-                                    android.R.layout.simple_list_item_1,
-                                    strAddresses
-                            );
-                            MainActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    listViewResTo.setAdapter(adapterSearchAddressResTo);
-                                }
-                            });
-
-                        } catch (IOException e) {
-                            Toast.makeText(context, "Ничего не найдено", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-                searchThread[0].start();
-                return false;
-            }
-        });
-    }
-
-    private Locale getRusLocal() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return new Locale.Builder().setLanguage("RU").setScript("Latn").setRegion("RS").build();
-        } else {
-            return new Locale("RU");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return result;
     }
 
-    private void createListViewListeners() {
-        listViewResFrom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                searchAddressFrom.setQuery(addressFrom[0].getAddressLine(0), false);
-                listViewResFrom.setAdapter(null);
+    private String getAddressFromPlace(Place place) {
+        String result = "";
+        Geocoder gcd = new Geocoder(context, new Locale("RU"));
+        try {
+            List<Address> addresses = gcd.getFromLocation(place.getLatLng().latitude,
+                    place.getLatLng().longitude, 1);
+            if (addresses.size() != 0) {
+                result = addresses.get(0).getThoroughfare() + " " + addresses.get(0).getSubThoroughfare();
             }
-        });
-
-        listViewResTo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                searchAddressTo.setQuery(addressTo[0].getAddressLine(0), false);
-                listViewResTo.setAdapter(null);
-            }
-        });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
-
-    private void createListeners() {
-        createSearchListeners();
-        createListViewListeners();
-    }
-
-    public Context getContext() { return context; }
 }
